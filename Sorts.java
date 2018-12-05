23932d93dae4f3bc58ffe495e5683dd5a40ad5e0
grace-mao23 <gmao00@stuy.edu>
2018-12-04 20:07:07 -0500

    Class created

---
+++ Sorts.java
@@ -0,0 +1,18 @@
+public class Sorts {
+
+  public static void selectionSort(int [] ary) {
+    for (int i = 0; i < ary.length; i++) {
+      int smallest = i;
+      for (int x = i; x < ary.length; x++) {
+        if (ary[x] < ary[smallest]) {
+          smallest = x;
+        }
+      }
+      int temp = ary[i];
+      ary[i] = ary[smallest];
+      ary[smallest] = temp;
+    }
+  }
+
+
+}
