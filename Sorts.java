749553f726e90391409aded7580d256e1b7016aa
grace-mao23 <gmao00@stuy.edu>
2018-12-07 14:34:57 -0500

    Bubble sort written

--- Sorts.java
+++ Sorts.java
@@ -14,6 +14,24 @@ public class Sorts {
     }
   }
 
+  public static void bubbleSort(int[] data){
+    boolean sorted = false;
+    while (!sorted) {
+      int swap = 0;
+      for (int i = 0; i < data.length - 1; i++) {
+        if (data[i] > data[i+1]) {
+          int temp = data[i];
+          data[i] = data[i+1];
+          data[i+1] = temp;
+          swap++;
+        }
+      }
+      if (swap == 0) {
+        sorted = false;
+      }
+    }
+  }
+
   public static void main(String[] args) {
     int[] array = {4, 2, 9, 1, 0, -4, 3, 8, 7, 3};
     Sorts.selectionSort(array);
